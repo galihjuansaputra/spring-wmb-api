3c6c07fa15a3190d20e0fce2e6b7f65aa27f40fc
Galih Prastya Juansaputra <galih.juansaputra@gmail.com>
2024-05-01 10:02:54 +0700

    [FIX] changing annotation

--- src/main/java/com/enigma/wmbapi/dto/request/NewCustomerRequest.java
+++ src/main/java/com/enigma/wmbapi/dto/request/NewCustomerRequest.java
@@ -2,13 +2,15 @@ package com.enigma.wmbapi.dto.request;
 
 import com.enigma.wmbapi.entity.Customer;
 import jakarta.validation.constraints.NotNull;
-import lombok.Builder;
-import lombok.Data;
+import lombok.*;
 
 /**
  * DTO for {@link Customer}
  */
-@Data
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
 @Builder
 public class NewCustomerRequest {
     @NotNull
